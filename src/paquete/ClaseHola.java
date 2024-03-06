74315f29a9360165377ad20fb68e11c2858116c1
ÁlvaroLozanoGarcía <alg888999@gmail.com>
2024-03-06 19:40:38 +0100

    Tercera versión master

--- src/paquete/ClaseHola.java
+++ src/paquete/ClaseHola.java
@@ -4,5 +4,6 @@ public class ClaseHola {
 public static void main(String[] args) {
 	System.out.println("Hola, mundo");
 	System.out.println("Hola a tod@s");
+	System.out.println("Contenido distinto en master");
 }
 }
